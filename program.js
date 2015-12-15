4918d5a30a2585f0c3ba13c4314ccb00392584a5
mosquito <102306072@nccu.edu.tw>
2015-12-15 12:31:03 +0800

    first commit

---
+++ program.js
@@ -0,0 +1,21 @@
+window.onload = init;
+
+function init() {
+//宣告資料庫們
+//顧客與銷售資料、生產相關參數、存貨與運輸參數
+}
+
+//生產
+function produce() {
+
+}
+
+//存貨
+function stock() {
+
+}
+
+//行銷
+function sell() {
+    
+}

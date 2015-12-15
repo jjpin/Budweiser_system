f0f5f45e6328d238362e3fe38e6acbb1bfb04627
郭泰竹 <102306037@nccu.edu.tw>
2015-12-15 17:27:38 +0800

    interface

--- program.js
+++ program.js
@@ -3,14 +3,38 @@ window.onload = init;
 function init() {
 //宣告資料庫們
 //顧客與銷售資料、生產相關參數、存貨與運輸參數
-//
+var produce=2000;//生產量預設2000公升
+var water=10000;//100000公升
+var sugar=100;//100噸
+var color=10000;//10000公克
+var co2=10000;//10000公升
+
+var amount_blue = 100;//每種產品的數量
+var amount_red = 100;
+var amount_orange = 100;
+var amount_black = 100;
 }
 
 //生產
-function produce() {
+function produce(name, amount) {
+	var amount;
+	var co2_amount = getElementById("amount_co2");
+	var sugar_amount = getElementById("amount_sugar");
+	var water_amount = getElementById("amount_water");
 
 }
+//跳窗調整原料量
+function ingredient(){
 
+}
+//跳窗調整製造量
+function p_amount(){
+
+}
+//跳窗新增產品
+function newproduct(){
+
+}
 //存貨
 function stock() {
 

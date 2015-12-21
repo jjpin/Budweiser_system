2ce8a3cba27d0c08d707f032ca07b51ecea4881d
mosquito <102306072@nccu.edu.tw>
2015-12-21 14:53:27 +0800

    存貨UI

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ window.onload = init;
 function init() {
 //宣告資料庫們
 //顧客與銷售資料、生產相關參數、存貨與運輸參數
-//
+
 }
 
 //生產
@@ -14,6 +14,10 @@ function produce() {
 //存貨
 function stock() {
 
+}
+
+function addstock(name) {
+    
 }
 
 //行銷

3416794cfc2ffe8e9a351eacfa08c221d276576d
mosquito <102306072@nccu.edu.tw>
2015-12-23 16:07:59 +0800

    新增訂單功能，調整預設存量

--- program.js
+++ program.js
@@ -12,10 +12,9 @@ function init() {
 
 
 var water=100000;//100000公升
-var sugar=100000;//公斤
-var color=100000;//公斤
-var co2=10000;//10000公升
-
+var sugar=1000;//公斤
+var color=1000;//公斤
+var co2=1000;//1000公升
 
 
 var store1_cur = [35, 10, 50]; //分店現在狀況(今日銷售, 剩餘存貨, 配送數量)
@@ -41,52 +40,9 @@ var store9_sales = [900, 1200, 1900, 1320, 1440, 1900, 2000, 2130, 1650, 1555, 9
 var store10_sales = [1600, 2500, 2640, 2505, 2830, 2900, 2990, 2600, 2350, 2194, 2000, 1500];
 
 
-//--------------
-//瀚：
-//泰竹你應該將這些商品的名稱與數量分別用array來儲存，
-//因為這是會變動的，產品會被新增或刪除，因此若是分開宣告一個個變數，你會很難去新增
-//所以我先宣告以下兩個array了，你記得將你的function們有用到商品數量的改一改
 productNames = ["藍色汽水","紅色汽水","橘色汽水","黑色汽水"];
-productAmounts = [2000,2000,2000,2000];
-//這樣使用者新增產品時直接使用push就搞定了，後面的存貨才方便存取，直接讀Array就好了
-//ex: productNames.push("林鳳營");
-//ex: productAmounts.push("100000000");
-//--------------
+productAmounts = [5000,5000,5000,5000];
 
-var store1_cur = [35, 10, 50]; //分店現在狀況(今日銷售, 剩餘存貨, 配送數量)
-var store2_cur = [28, 20, 50];
-var store3_cur = [34, 9, 50];
-var store4_cur = [8, 32, 50];
-var store5_cur = [22, 20, 50];
-var store6_cur = [39, 5, 50];
-var store7_cur = [19, 25, 50];
-var store8_cur = [20, 20, 50];
-var store9_cur = [15, 30, 50];
-var store10_cur = [40, 5, 50];
-
-var store1_sales = [1000, 1500, 2100, 1505, 1830, 1900, 2100, 2200, 2250, 1894, 1200, 1100]; //分店1的各月銷售量
-var store2_sales = [1600, 2500, 2640, 2505, 2830, 2900, 2990, 2600, 2350, 2194, 2000, 1500];
-var store3_sales = [1100, 1300, 1900, 2005, 1800, 2100, 2300, 2200, 2250, 1872, 1400, 1300];
-var store4_sales = [900, 1200, 1900, 1320, 1440, 1900, 2000, 2130, 1650, 1555, 990, 880];
-var store5_sales = [1100, 1300, 1900, 2005, 1800, 2100, 2300, 2200, 2250, 1872, 1400, 1300];
-var store6_sales = [1000, 1500, 2100, 1505, 1830, 1900, 2100, 2200, 2250, 1894, 1200, 1100];
-var store7_sales = [1100, 1300, 1900, 2005, 1800, 2100, 2300, 2200, 2250, 1872, 1400, 1300];
-var store8_sales = [1000, 1500, 2100, 1505, 1830, 1900, 2100, 2200, 2250, 1894, 1200, 1100];
-var store9_sales = [900, 1200, 1900, 1320, 1440, 1900, 2000, 2130, 1650, 1555, 990, 880];
-var store10_sales = [1600, 2500, 2640, 2505, 2830, 2900, 2990, 2600, 2350, 2194, 2000, 1500];
-
-
-//--------------
-//瀚：
-//泰竹你應該將這些商品的名稱與數量分別用array來儲存，
-//因為這是會變動的，產品會被新增或刪除，因此若是分開宣告一個個變數，你會很難去新增
-//所以我先宣告以下兩個array了，你記得將你的function們有用到商品數量的改一改
-productNames = ["藍色汽水","紅色汽水","橘色汽水","黑色汽水"];
-productAmounts = ["100","100","100","100"];
-//這樣使用者新增產品時直接使用push就搞定了，後面的存貨才方便存取，直接讀Array就好了
-//ex: productNames.push("林鳳營");
-//ex: productAmounts.push("100000000");
-//--------------
 
 //生產要素
 var pamount
@@ -99,10 +55,9 @@ var col_ingredient=50;
 var ingredientStats = ["正常","正常","正常","正常"];
 var productStats = ["正常","正常","正常","正常"];
 
-
-//存貨狀態
-var ingredientStats = ["正常","正常","正常","正常"];
-var productStats = ["正常","正常","正常","正常"];
+//補貨相關
+var ingredientOrderAmount = [100000,1000,1000,1000];
+var addStockRequestIndex = 0;
 
 //生產
 function sureforproduce(){
@@ -133,12 +88,7 @@ function produce(name,amount) {
 	
 	alert("您所製造的品項為:"+ name +"，製造量為:"+ amount +"L");
 	
-
-	//瀚：生產完了原料與商品數量的變數都沒有變啊～～～～
 	stockRefresh();
-
-	//瀚：生產完了原料與商品數量的變數都沒有變啊～～～～
-
 
 }
 //跳窗調整原料量(按比例)
@@ -216,9 +166,6 @@ function addOption(list, text, value){
 	list.options[index]=new Option(text, value);
 	list.selectedIndex=index;
 
-	//瀚：
-	//這裡只有list裡新增，但系統裡根本沒有真正的新增到～～
-	//所以幫你做了
 	productNames.push(value);
 	productAmounts.push(0);
 	productStats.push("短缺");
@@ -228,6 +175,62 @@ function addOption(list, text, value){
 }
 //存貨
 function stock() {
+
+}
+
+//存貨向生產送出訂單
+function addStockRequest(id) {
+	addStockRequestIndex++;
+	var stockRequestBox = document.createElement("div");
+    document.getElementById("requestBoxContainer").appendChild(stockRequestBox);
+    stockRequestBox.id = "requestBox" + addStockRequestIndex;
+    stockRequestBox.setAttribute("class", "requestBox");
+    stockRequestBox.setAttribute("needid", id);
+    stockRequestBox.onclick = addStockConfirm;
+    var tempAmountArray = ["水","二氧化碳","糖","色素"];
+    if(id.slice(0,1) == "i") {
+    	var name = tempAmountArray[id.slice(1,2)-1];
+    	stockRequestBox.innerHTML = name + "存貨短缺，存貨端要求補貨";
+
+    } else {
+    	var name = productNames[id.slice(1,2)-1];
+    	stockRequestBox.innerHTML = name + "存貨短缺，存貨端要求生產";
+    }
+    alert("已送出訂單");
+}
+
+//生產確認訂單
+function addStockConfirm(eventObj) {
+	var boxid = eventObj.target.id;
+	var needid = eventObj.target.getAttribute("needid");
+	var tempAmountArray = ["水","二氧化碳","糖","色素"];
+	if(needid.slice(0,1) == "i") {
+    	var name = tempAmountArray[needid.slice(1,2)-1];
+    	if(confirm("是否補貨" + name + ingredientOrderAmount[needid.slice(1,2)-1] + "單位？")){
+    		if(needid.slice(1,2) == 1) {
+    			water += 100000;
+    		} else if (needid.slice(1,2) == 2) {
+    			co2 += 1000;
+    		} else if (needid.slice(1,2) == 3) {
+    			sugar += 1000;
+    		} else {
+    			color += 1000;
+    		}
+    		alert("補貨完成！");
+    		var del = document.getElementById(boxid);
+    		del.parentNode.removeChild(del);
+    	}
+    } else {
+    	var name = productNames[needid.slice(1,2)-1];
+    	if(confirm("是否生產" + name + "5000單位？")){
+    		produce(name,5000);
+    		var del = document.getElementById(boxid);
+    		del.parentNode.removeChild(del);
+    	}
+    }
+    stockRefresh();
+    
+	
 
 }
 
@@ -249,7 +252,7 @@ function addStockBox(name) {
     document.getElementsByName("productStatSample")[1].removeAttribute("name");
     document.getElementsByName("pS")[1].id = "p" + productNames.length;
     document.getElementsByName("pS")[1].removeAttribute("name");
-    document.getElementsByName("addStockButton")[1].onclick = "addStock(p" + productNames.length + ".id)" ;
+    document.getElementsByName("addStockButton")[1].setAttribute("onclick", "addStockRequest(p" + productNames.length + ".id)");
     document.getElementsByName("addStockButton")[1].removeAttribute("name");
 
     stockRefresh();
